e0deb0275587b1f5f7cfd624c4f9aa384161b5fc
bawki <priivet@gmail.com>
2020-04-14 20:31:47 +0200

    version 0.3
    added multilab import
    fixed cleaning data between imports

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,8 +6,11 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->comboBox->addItem("Komma");
+    ui->comboBox->addItem("Punkt");
+    ui->comboBox->setCurrentIndex(0);
     ui->tableWidget->setColumnCount(55);
-    ui->tableWidget->setRowCount(1);
+
     QStringList headers;
     headers << "Leukozyten" << "Haemoglobin" << "Thrombozyten" << "Lymphozyten" << "Eosinophile" << "INR" << "aPTT" << "D-Dimere" << "TZ" << "Natrium" << "Kalium" << "Albumin" << "Bilirubin(ges)"
             << "Bilirubin(dir)" << "AST" << "ALT" << "gGT" << "AP" << "LDH" << "CK" << "CK-MB" << "TroponinT" << "NTproBNP" << "Harnstoff" << "Kreatinin" << "CRP" << "Procalcitonin"
@@ -78,8 +81,22 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+
+}
+
+void MainWindow::on_loadLabButton_clicked()
+{
     QString data = ui->textEdit->toPlainText();
     QStringList strList = data.split(QRegExp("[\n]"),QString::SkipEmptyParts);
+    int lastRow = ui->tableWidget->rowCount();
+    qDebug() << "rowCount before insert is: " << lastRow;
+    ui->tableWidget->insertRow(lastRow);
+    qDebug() << "rowCount before insert is: " << ui->tableWidget->rowCount();
 
     foreach(QString str, strList) {
         QStringList split = str.split("	");
@@ -87,6 +104,11 @@ void MainWindow::on_pushButton_clicked()
             continue;
         QString p = split[0];
         QString v= split[2];
+        if(ui->comboBox->currentIndex()== 0){
+            v = v.replace(".",",");
+        } else {
+            v = v.replace(",",".");
+        }
 
         if(v.contains(QRegExp("[+-]?([0-9]*[.])?[0-9]+")) || v == "folgt") {
             qDebug() << "p: " << p << "v: " << v;
@@ -94,19 +116,22 @@ void MainWindow::on_pushButton_clicked()
             if(idx>=0) {
                 qDebug() << "found idx: " << idx;
                 QTableWidgetItem *item = new QTableWidgetItem(v);
-                ui->tableWidget->setItem(0,idx,item);
+                ui->tableWidget->setItem(lastRow,idx,item);
             }
         }
     }
 }
 
-void MainWindow::on_pushButton_2_clicked()
+void MainWindow::on_copyLabButton_clicked()
 {
     QList<QTableWidgetItem*> selected = QList<QTableWidgetItem*>();
     QByteArray items;
+    qDebug() << "onCopy: roWcount is: " << ui->tableWidget->rowCount();
     for(int i = 0; i< ui->tableWidget->columnCount(); i++) {
+
         QTableWidgetItem* t = ui->tableWidget->item(0,i);
         if(t == nullptr) {
+            //qDebug() << "t is null!";
             items.append("");
         } else {
             items.append(t->text());
@@ -117,4 +142,79 @@ void MainWindow::on_pushButton_2_clicked()
     QMimeData *mimeData = new QMimeData();
     mimeData->setData("text/plain",items);
     QApplication::clipboard()->setMimeData(mimeData);
+}
+
+void MainWindow::on_clearButton_clicked()
+{
+    for(int i = ui->tableWidget->rowCount(); i >=0; i--){
+        ui->tableWidget->removeRow(i);
+    }
+    ui->textEdit->clear();
+}
+
+
+
+
+
+void MainWindow::on_loadMultiButton_clicked()
+{
+    QString data = ui->textEdit->toPlainText();
+    QStringList strList = data.split(QRegExp("[\n]"),QString::SkipEmptyParts);
+    int labCount = strList.at(0).split("\t").length();
+    qDebug() << "multi has: " << labCount-3 << " labs";
+    ui->tableWidget->setRowCount(labCount);
+    int curRow = 0;
+    foreach(QString str, strList) {
+        qDebug() << str;
+        QStringList split = str.split("\t");
+        QString p = split[0];
+        for(int i=4;i<split.length();i++) {
+            QString v = split[i];
+            if(ui->comboBox->currentIndex()== 0){
+                v = v.replace(".",",");
+            } else {
+                v = v.replace(",",".");
+            }
+            if(v.contains(QRegExp("[+-]?([0-9]*[.])?[0-9]+")) || v == "folgt") {
+                v = v.simplified().remove(" ");
+                v = v.remove("<").remove(">").remove("+").remove("-");
+
+                qDebug() << "p: " << p << "v: " << v;
+                int idx = indexes.value(p, -1);
+                if(idx>=0) {
+                    qDebug() << "found idx: " << idx;
+                    QTableWidgetItem *item = new QTableWidgetItem(v);
+                    ui->tableWidget->setItem(curRow,idx,item);
+                }
+            }
+            curRow++;
+        }
+        curRow = 0;
+    }
+    for(int r = ui->tableWidget->rowCount()-1; r>=0; r-- ) {
+        bool hasValue = false;
+        for(int c = 0; c< ui->tableWidget->columnCount(); c++) {
+            QTableWidgetItem* item = ui->tableWidget->item(r,c);
+            if(item != nullptr){
+                hasValue = true;
+            }
+            /*if(item->text().length()>0){
+                qDebug() << "row: " << r << " has non-empty value: " << item->text() << " at column: " << c;
+                hasValue = true;
+            }*/
+        }
+        if(!hasValue){
+            qDebug() << "removing row: " << r;
+            ui->tableWidget->removeRow(r);
+        }
+    }
+}
+
+void MainWindow::on_tableWidget_customContextMenuRequested(const QPoint &pos)
+{
+    QTableWidgetItem* item = ui->tableWidget->itemAt(pos);
+    if(item == nullptr){
+        qDebug() << " no item at this position";
+        return;
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -23,6 +23,16 @@ private slots:
 
     void on_pushButton_2_clicked();
 
+    void on_clearButton_clicked();
+
+    void on_loadLabButton_clicked();
+
+    void on_copyLabButton_clicked();
+
+    void on_loadMultiButton_clicked();
+
+    void on_tableWidget_customContextMenuRequested(const QPoint &pos);
+
 private:
     Ui::MainWindow *ui;
     QMap<QString, int> indexes;
